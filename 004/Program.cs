48fc353e95fb9b426c230d7ff0f152ebee835d5c
Sys Darya <sysdaryaandreevna@gmail.com>
2022-08-16 21:08:12 +0300

    004 task

---
+++ 004/Program.cs
@@ -0,0 +1,35 @@
+﻿// 
+
+void Fill (int[] M){
+    for (int p = 0; p < M.Length; p++){
+        M[p] = new Random().Next(-90,91);
+    }
+}
+void Print(int[] M)
+{
+    foreach(var element in M)
+        Console.Write($"{element} ");
+}
+
+bool Contains(int[] M, int value)
+{
+    foreach(var element in M)
+        if(element==value)
+            return true;
+    return false;
+}
+Console.Write("Введите размер массива: " );
+int size = int.Parse(Console.ReadLine() ?? "0");
+
+int[] array = new int[size];
+Fill(array);
+
+Console.Write("Ваш массив: ");
+Print (array);
+Console.WriteLine(" ");
+
+Console.Write("Введите элемент, котрый хотите найти: ");
+int element = int.Parse(Console.ReadLine() ?? "0");
+bool contains = Contains(array,element);
+Console.WriteLine(contains ? "Элемент присутствует." : 
+    "Элемента нет :(");
